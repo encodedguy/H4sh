4c8aafaa5e539a8f84847584461132076760eb92
Inderjeet Singh <singhinder1208@gmail.com>
2021-11-09 16:00:26 +0530

    MD5 class added and fixed Switch method in H4sh class

--- H4sh.java
+++ H4sh.java
@@ -1,10 +1,11 @@
 class H4sh{
-	public String Switch(String str)
+	public static void Switch(String str)
 	{
 		switch (str){
 			case "A":	
-				base32 base32Obj = new base32();
-				base32Obj.base32_1();
+				//base32 base32Obj = new base32();
+				//base32Obj.base32_1();
+				break;
 			case "B":
 				//base 64 //
 			case "C":
@@ -14,7 +15,9 @@ class H4sh{
 			case "E":
 				//NTLM//
 			case "F":
-				//MD5//
+				md5 md5Obj = new md5();
+				md5Obj.printHash();
+				break;
 			case "G":
 				//SHA128//
 			case "H":
@@ -28,43 +31,42 @@ class H4sh{
 			case "L":
 				//Caesar Cipher//
 			default:
-				//any statement//
-				
-					
+				//any statement//		
+		}				
 	}
 	public static void main(String[] args){
 		banner bannerObj  = new banner();
-		
+		H4sh h4shObj = new H4sh();
+
 		if (bannerObj.numInput() == 1){
 			bannerObj.banner1();
-			Switch(bannerObj.banner1Input());
+			H4sh.Switch(bannerObj.banner1Input());
 			
 		}
 	
 		if (bannerObj.numInput() == 2){
 			bannerObj.banner1();
-			Switch(bannerObj.banner1Input());
+			H4sh.Switch(bannerObj.banner1Input());
 			
 		}
 	
 		if (bannerObj.numInput() == 3){
 			bannerObj.banner1();
-			Switch(bannerObj.banner1Input());
+			H4sh.Switch(bannerObj.banner1Input());
 			
 		}
 		if (bannerObj.numInput() == 4){
 			bannerObj.banner1();
-			Switch(bannerObj.banner1Input());
-			
+			H4sh.Switch(bannerObj.banner1Input());
 		}
 		if (bannerObj.numInput() == 5){
 			bannerObj.banner1();
-			Switch(bannerObj.banner1Input());
+			H4sh.Switch(bannerObj.banner1Input());
 			
 		}
 		if (bannerObj.numInput() == 6){
 			bannerObj.banner1();
-			Switch(bannerObj.banner1Input());
+			H4sh.Switch(bannerObj.banner1Input());
 			
 		}
 	}
