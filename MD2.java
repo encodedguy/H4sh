72e5384f79e4072a1f1052ef3b242a15732d8efe
Sakshi Singh Dangi <78265680+sakshisingh12@users.noreply.github.com>
2021-11-17 18:03:35 +0530

    Add files via upload

--- MD2.java
+++ MD2.java
@@ -1,27 +1,20 @@
-import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigInteger;
 
 class MD2{
     public MD2(){
         System.out.print("\033[H\033[2J");  
 		System.out.flush(); 
-		System.out.println("BASE64 MODE");
-		encode();
+		System.out.println("MD2 ENCRYPTION MODE");
     }
 
-    public String takeInput(){
-        System.out.printf("Enter a string: ");
-        Scanner input = new Scanner(System.in);
-        return input.nextLine();
-    }
-
-    public String encode(){
-        String password = takeInput();
+    public String encode(String password){
         final byte[] defaultBytes = password.getBytes();
         try {
-
             MessageDigest md = MessageDigest.getInstance("MD2");
             byte[] messageDigest = md.digest(defaultBytes);
             BigInteger no = new BigInteger(1, messageDigest);
@@ -36,7 +29,8 @@ class MD2{
         }
     }
 
-    public void printHash(){
-        System.out.println("MD2: " + encode());
-    }
+    public void printHash(int option){
+		Input input = new Input();
+		System.out.println("MD5: " + encode(input.takeInput(option)));
+	}
 }
